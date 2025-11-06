36af4ddef8e7260fb0adbe526b202a25210864a0
Jared sbt <jaredsbt@gmail.com>
2025-11-05 20:29:02 -0800

    streak model

--- src/game/models/AnswerStreak.ts
+++ src/game/models/AnswerStreak.ts
@@ -3,13 +3,8 @@ type StreakState = "idle" | "building" | "active" | "cooldown";
 export class AnswerStreak {
   state: StreakState = "idle"; // Current state of streak
   gauge: number = 0; // 0 → 100 fill level
-  timer: number = 0; // Remaining streak time (if active)
-  decayRate: number = 0.5; // How fast gauge drains per second
-  fillPerCorrect: number = 25; // How much gauge increases per correct answer
-
-  constructor() {}
-
-  update(deltaTime: number) {}
+  time: number = 0; // Remaining streak time (if active)
+  decayRate: number = 5; // How fast gauge drains per second
 
   onCorrectAnswer() {
     if (this.gauge === 100) {
@@ -20,15 +15,28 @@ export class AnswerStreak {
   }
 
   onWrongAnswer() {
-    this.gauge == 0;
-    this.state = "cooldown";
+    this.deactivateStreak();
   }
 
   activateStreak() {
     this.state = "active";
+    this.time = 10;
+    const timer = setInterval(() => {
+      console.log(this.time);
+      this.time--;
+      this.gauge -= this.decayRate;
+
+      if (this.time < 0) {
+        clearInterval(timer); // stop the timer
+        this.deactivateStreak();
+        console.log("Time’s up!");
+      }
+    }, 1000); // runs every 1000 ms = 1 second
   }
 
   deactivateStreak() {
     this.state = "idle";
+    this.time = 0;
+    this.gauge = 0;
   }
 }
